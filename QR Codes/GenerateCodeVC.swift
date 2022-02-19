24d22faf51bc2c21296bda90063cad94ad403564
eslamNahel <eslamoz.nasser@gmail.com>
2022-02-19 14:16:29 +0200

    Remove: remove storyboard

---
+++ QR Codes/GenerateCodeVC.swift
@@ -0,0 +1,16 @@
+//
+//  GenerateCodeVC.swift
+//  QR Codes
+//
+//  Created by Eslam Nahel on 19/02/2022.
+//
+
+import UIKit
+
+class GenerateCodeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+}
